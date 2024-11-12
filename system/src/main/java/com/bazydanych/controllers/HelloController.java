7cfad60dffaf9ffe598b11f48c8e05d16fd59fe7
Jakub Kapłonek <kuba.kaplonek@gmail.com>
2024-11-12 14:35:42 +0100

    Add vue router

--- system/src/main/java/com/bazydanych/controllers/HelloController.java
+++ system/src/main/java/com/bazydanych/controllers/HelloController.java
@@ -1,11 +1,13 @@
 package main.java.com.bazydanych.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import main.java.com.bazydanych.hotel.model.Room;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 	@GetMapping("/")
 	public Room handle() {
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,5 +1,27 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue/dist/vue.esm-bundler'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const Home = {template: '<div>Home</div>'};
+const About = {template: '<div>About</div>'};
+const Profile = {template: '<div>Profile</div>'};
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+    { path: '/profile', component: Profile },
+]
+  
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
+})
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app')
